00c94c60248fcb861643cf61eec070d97bd27dae
f-tozzini <31866568+f-tozzini@users.noreply.github.com>
2018-03-06 22:42:36 -0500

    setting up kid's layout and general content

---
+++ db_app.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 07, 2018 at 03:55 AM
+-- Server version: 5.6.35
+-- PHP Version: 7.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_movies`
+--
+
+CREATE TABLE `tbl_movies` (
+  `movies_id` smallint(5) UNSIGNED NOT NULL,
+  `movies_title` varchar(100) NOT NULL,
+  `movies_thumbs` varchar(200) NOT NULL,
+  `movies_video` varchar(200) NOT NULL,
+  `movies_desc` varchar(1000) NOT NULL,
+  `movies_genre` varchar(30) NOT NULL,
+  `movies_year` varchar(20) NOT NULL,
+  `movies_rating` varchar(20) NOT NULL,
+  `movies_classification` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_movies`
+--
+
+INSERT INTO `tbl_movies` (`movies_id`, `movies_title`, `movies_thumbs`, `movies_video`, `movies_desc`, `movies_genre`, `movies_year`, `movies_rating`, `movies_classification`) VALUES
+(1, 'Beauty and the Beast', 'beauty-beast.jpg', 'beauty-beast.mov', 'An adaptation of the fairy tale about a monstrous-looking prince and a young woman who fall in love.', 'Drama', '2017', '73', 'Kids'),
+(2, 'Dark knight', 'batman.jpg', 'batman.mp4', 'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham, the Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.', 'Action', '2008', '82', 'Adult'),
+(3, 'Wanted', 'wanted.jpg', 'wanted.mp4', 'A frustrated office worker learns that he is the son of a professional assassin and that he shares his father\'s superhuman killing abilities.', 'Action', '2008', '67', 'Adult'),
+(4, 'The avengers', 'avengers.jpg', 'avengers.mp4', 'Earth\'s mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.', 'Action', '2012', '81', 'Adult'),
+(5, 'Amelie', 'amelie.jpg', 'amelie.mp4', 'Amélie is an innocent and naive girl in Paris with her own sense of justice. She decides to help those around her and, along the way, discovers love.', 'Drama', '2001', '83', 'Adult'),
+(6, 'Inside out', 'inside-out.jpg', 'inside-out.mp4', 'After young Riley is uprooted from her Midwest life and moved to San Francisco, her emotions - Joy, Fear, Anger, Disgust and Sadness - conflict on how best to navigate a new city, house, and school.', 'Animation', '2015', '82', 'Kids'),
+(7, 'Up!', 'up.jpg', 'up.mp4', 'Seventy-eight year old Carl Fredricksen travels to Paradise Falls in his home equipped with balloons, inadvertently taking a young stowaway.', 'Animation', '2009', '83', 'Kids'),
+(8, 'Frozen', 'frozen.jpg', 'frozen.mp4', 'When the newly-crowned Queen Elsa accidentally uses her power to turn things into ice to curse her home in infinite winter, her sister Anna teams up with a mountain man, his playful reindeer, and a snowman to change the weather condition.', 'Animation', '2013', '75', 'Kids'),
+(9, 'Mulan', 'mulan.jpg', 'mulan.mp4', 'To save her father from death in the army, a young maiden secretly goes in his place and becomes one of China\'s greatest heroines in the process.', 'Animation', '1998', '76', 'Kids'),
+(10, 'Toy Story', 'toy-story.jpg', 'toy-story.mp4', 'A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy\'s room.', 'Animation', '2003', '81', 'Kids'),
+(11, 'Gilmore Girls', 'gimore-girls.jpg', 'gimore-girls.mp4', 'A dramedy centering around the relationship between a thirtysomething single mother and her teen daughter living in Stars Hollow, Connecticut.', 'TV Show', '2000-2007', '81', 'Adult'),
+(12, 'One Tree Hill', 'one-tree-hill.jpg', 'one-tree-hill.mp4', 'Half-brothers Lucas and Nathan Scott trade between kinship and rivalry both on the basketball court and in the hearts of their friends in the small, but not so quiet town of Tree Hill, North Carolina.', 'TV Show', '2003-2012', '77', 'Adult'),
+(13, 'Outlander', 'outlander.jpg', 'outlander.mp4', 'An English combat nurse from 1945 is mysteriously swept back in time to 1743.', 'TV Show', '2014-2018', '85', 'Adult'),
+(14, 'Finding Nemo', 'finding-nemo.jpg', 'finding-nemo.mp4', 'After his son is captured in the Great Barrier Reef and taken to Sydney, a timid clownfish sets out on a journey to bring him home.', 'Animation', '2003', '81', 'Kids');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_movies`
+--
+ALTER TABLE `tbl_movies`
+  ADD PRIMARY KEY (`movies_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_movies`
+--
+ALTER TABLE `tbl_movies`
+  MODIFY `movies_id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,37 @@ var config = require('../config');
 var toRender = (config.kidsmode) ? 'kids' : 'main ';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render(toRender, {
-    title: 'HOME Page!!',
-    message: 'Heloooo!!',
-    mainpage: true,
-    kidsmode : config.kidsmode
-  });
+// router.get('/', function(req, res, next) {
+//   res.render(toRender, {
+//     title: 'HOME Page!!',
+//     message: 'Heloo!!',
+//     mainpage: true,
+//     kidsmode : config.kidsmode
+//   });
+// });
+
+// router.get('/', (req, res) => {
+//   console.log('hit the main route');
+//   res.render('main');
+// });
+
+router.get('/', (req, res) => {
+  console.log('hit the home route');
+  res.render('home');
+  kids : false
+});
+
+router.get('/adults', (req, res) => {
+  console.log('hit the adults route');
+  res.render('adults');
+  kids : false
+});
+
+router.get('/kids', (req, res) => {
+  console.log('hit the kids route');
+  res.render('kids', {
+    kids : true
+  })
 });
 
 module.exports = router;
